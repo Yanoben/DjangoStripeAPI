9899e469259e8d71848a9a9ebee81670feb43b8c
Ghost <yasinabdurakhmanov@gmail.com>
2022-09-09 21:31:13 +0300

    create model Item

--- stripe_api/stripe/models.py
+++ stripe_api/stripe/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField()
+    description = models.TextField(max_length=100)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
